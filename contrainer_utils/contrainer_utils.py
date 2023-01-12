33bfb953066e99cb9c508a79b32f1b694b518dc8
Musa <musa@stride.one>
2023-01-12 23:30:07 +0300

    ! content-type

--- contrainer_utils/contrainer_utils.py
+++ contrainer_utils/contrainer_utils.py
@@ -88,7 +88,7 @@ def put_image(bucket, name, content):
         Body=content,
         Bucket=bucket,
         Key=name,
-        ContentType='application/octet-stream'
+        ContentType='image/jpeg'
     )
 
 
